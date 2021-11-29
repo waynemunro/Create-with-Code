774970d38e9b21a85b247a0d218f73cd38ab7b6f
Wayne Munro <964342+waynemunro@users.noreply.github.com>
2021-11-29 16:32:34 +0200

    vehicle controlable

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed = 10.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 20.0f;
+    private float turnspeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Update is called once per frame
     private void Update()
     {
-        // We'll move the vehicle forward
+        // this is where we get player imput
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+        // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // we turn the vehicle
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
     }
 }
